7058fed77435d8163383303bc1359b77af61aee9
shevek27 <jportega02@gmail.com>
2025-04-01 19:03:18 -0300

    implementada las funciones para crear carpetas segun la facultad y materia y bajar cada respuesta de la url. estoy sacando todo el html de las respuestas, la mayoria de los examenes son una imagen en formato .gif. proximo paso bajar solo eso.

--- scrapper_examen.py
+++ scrapper_examen.py
@@ -1,3 +1,6 @@
+# create folders/write files
+import os
+# urls and html
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -6,7 +9,7 @@ def get_asp_links(url):
 
     # response -> soup object
     response = requests.get(url)
-    # check for errors (404,403,etc)
+    # git
     response.raise_for_status() 
     soup = BeautifulSoup(response.text, 'html.parser')
     
@@ -23,10 +26,55 @@ def get_asp_links(url):
     
     return asp_links
 
-target_url = "https://www.altillo.com/Examenes/uba/exactas/index.asp"
-links = get_asp_links(target_url)
 
-for index, link in enumerate(links, 1):
-    print(f"{index}. {link}")
+def get_save_path(link):
+    components = link.split('/')
+    # uba url example: https://www.altillo.com/Examenes/uba/exactas/algebra/alg_2015_2pb.asp
+    # normal url example: https://www.altillo.com/Examenes/unlp/logica/log_2015_2pa.asp
+    exam_name = components[-1]
+    materia = components[-2]
+    facultad = components[-3]
+    uba = ""
+    if components[-4] == "uba":
+        uba = "uba"
+    
 
-print(f"se encontraron {len(links)} examenes!")
+    save_path = os.path.join(uba, facultad, materia)
+    
+    return save_path
+
+
+
+def write_exams(links, save_path):
+    # create folder
+
+    os.makedirs(save_path, exist_ok=True)
+    
+    # write each exam
+    for link in links:
+        components = link.split('/')
+        exam_name = components[-1]
+        response = requests.get(link)
+        full_path = os.path.join(save_path, exam_name)
+        with open(full_path, "wb") as exam_file:
+            exam_file.write(response.content)
+        print(f"downloaded exam: {exam_name}")
+
+
+def main():
+    target_url = "https://www.altillo.com/Examenes/uba/exactas/index.asp"
+    links = get_asp_links(target_url)
+
+    for index, link in enumerate(links, 1):
+        print(f"{index}. {link}")
+
+    print(f"se encontraron {len(links)} examenes!")
+    print("wtf")
+    save_path = get_save_path(links[0])
+    write_exams(links, save_path)
+
+    
+
+
+if __name__ == "__main__":
+    main()
