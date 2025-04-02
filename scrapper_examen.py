fc59802085be3a54d2531bb1a1d2e0b837cabaa5
shevek27 <jportega02@gmail.com>
2025-04-02 01:18:20 -0300

    i thought the best way to do it was downloading the .gif image in most .asp pages as a .jpeg. The problem is that sometimes most of the exam is in text and with more than one image. This makes it harder. I could find out what pages are like that and then download the whole page, but i think its better if i just download the .asp or something locally in the future.

--- scrapper_examen.py
+++ scrapper_examen.py
@@ -4,6 +4,11 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+# convert from .gif to png
+from PIL import Image
+from io import BytesIO
+
+# tips: [url].asp .gif images are actually in [url]/[filename].gif . we have to remove the .asp
 
 def get_asp_links(url):
 
@@ -25,6 +30,73 @@ def get_asp_links(url):
                     asp_links.append(full_url)
     
     return asp_links
+
+
+# exams are images in .gif format
+def get_exam_image_url(asp_link):
+    try:
+        response = requests.get(asp_link)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for img in soup.find_all("img", src=True):                
+            image_link_list = img["src"].split('/')
+            if len(image_link_list) > 1:
+                image_link = image_link_list[1]
+            else:
+                image_link = image_link_list[0]
+            print(image_link)
+            if image_link.lower().endswith(".gif"):
+                asp_link = asp_link.replace(".asp", "")
+                full_url = asp_link + "/" + image_link
+                #print(f"asp_link={asp_link}, image_link= {image_link}, full_url = {full_url}")
+                return full_url
+
+    except Exception as e:
+        print(f"error with file {asp_link}, error= {e}")
+
+
+def download_image_as_jpg(image_url, save_folder, save_as="jpg", quality=100):
+    filename = image_url
+    name_without_extension = os.path.splitext(filename)[0]
+    name_with_desired_extension = f"{name_without_extension}.{save_as}"
+    save_path = os.path.join(save_folder, name_with_desired_extension)
+    if os.path.exists(save_path):
+        print("image already exists!")
+        return
+
+    else:   
+        try:
+            response = requests.get(image_url, stream=True)
+
+            # data to Pillow
+            img = Image.open(BytesIO(response.content))
+            # convert to RGB (.GIF uses 'P' palette mode)
+            if save_as.lower() != "gif":
+                if img.mode in ("RGBA", 'P'):
+                    img = img.convert("RGB")
+
+                save_options = {}   
+
+                if save_as.lower() == "jpg":
+                    save_options["quality"] = quality
+                    img.save(save_path, format="JPEG", quality=quality)
+                print("no error here")
+
+        except Exception as e:
+            print(f"error! {e}")
+
+
+def save_as_asp(url, save_path):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # save as HTML 
+        base_name = os.path.bas(url)
+
+    except Exception as e:
+        print(f"error! {e}")
+        return
+
 
 
 def get_save_path(link):
@@ -52,14 +124,8 @@ def write_exams(links, save_path):
     
     # write each exam
     for link in links:
-        components = link.split('/')
-        exam_name = components[-1]
-        response = requests.get(link)
-        full_path = os.path.join(save_path, exam_name)
-        with open(full_path, "wb") as exam_file:
-            exam_file.write(response.content)
-        print(f"downloaded exam: {exam_name}")
-
+        full_exam_url = get_exam_image_url(link)
+        download_image_as_jpg(full_exam_url, save_path)
 
 def main():
     target_url = "https://www.altillo.com/Examenes/uba/exactas/index.asp"
